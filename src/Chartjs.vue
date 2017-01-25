89d7e7ec5e0d28ddb2395fbe73bf8ab8ad315af8
mdorta-hiades <mdorta@hiades.es>
2017-01-25 14:39:55 +0000

    tabs to spaces

--- src/Chartjs.vue
+++ src/Chartjs.vue
@@ -1,5 +1,5 @@
 <template>
-	<canvas class="chartjs" :width="width" :height="height"></canvas>
+  <canvas class="chartjs" :width="width" :height="height"></canvas>
 </template>
 
 <script>
@@ -9,68 +9,68 @@ const types = ['line', 'bar', 'radar', 'polarArea', 'pie', 'doughnut']
 
 export default {
 
-	props: {
-		width: Number,
-		height: Number,
-		type: {
-			type: String,
-			required: true,
-			validator (value) {
-				return types.indexOf(value) > -1
-			}
-		},
-		data: {
-			type: Object,
-			required: true,
-			default: () => ({})
-		},
-		options: {
-			type: Object,
-			default: () => ({})
-		}
-	},
+  props: {
+    width: Number,
+    height: Number,
+    type: {
+      type: String,
+      required: true,
+      validator (value) {
+        return types.indexOf(value) > -1
+      }
+    },
+    data: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
 
-	mounted () {
-		this.chart = new Chart(this.$el, {
-			type: this.type,
-			data: this.data,
-			options: this.options
-		})
-	},
+  mounted () {
+    this.chart = new Chart(this.$el, {
+      type: this.type,
+      data: this.data,
+      options: this.options
+    })
+  },
 
-	data () { return {
-		chart: null
-	}},
+  data () { return {
+    chart: null
+  }},
 
-	methods: {
-		resetChart() {
-			this.$nextTick(() => {
-				this.chart.destroy()
-				this.chart = new Chart(this.$el, {
-					type: this.type,
-					data: this.data,
-					options: this.options
-				})
-			})
-		}
-	},
+  methods: {
+    resetChart() {
+      this.$nextTick(() => {
+        this.chart.destroy()
+        this.chart = new Chart(this.$el, {
+          type: this.type,
+          data: this.data,
+          options: this.options
+        })
+      })
+    }
+  },
 
-	watch: {
-		type (val) {
-			this.resetChart()
-		},
-		data (val) {
-			this.resetChart()
-		},
-		options (val) {
-			this.resetChart()
-		}
-	}
+  watch: {
+    type (val) {
+      this.resetChart()
+    },
+    data (val) {
+      this.resetChart()
+    },
+    options (val) {
+      this.resetChart()
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 canvas.chartjs {
-	max-width: 100%;
+  max-width: 100%;
 }
 </style>
